c1ae0d2adaa117137577e683b186c7ba7d6884bf
Adam Harvey <heyitsmeharv@gmail.com>
2018-09-11 11:16:53 +0100

    text done. need grammar check

--- src/pages/portfolio/Portfolio.js
+++ src/pages/portfolio/Portfolio.js
@@ -19,23 +19,30 @@ class Portfolio extends Component {
           <img src={Mcloud} alt="Gassy Mcloud" class="image"/>
           <div class="overlay">
             <div class="header">Gassy Mcloud</div>
-            <div className="text">Gassy Mcloud was my first mobile project made in C++ using Cocos2D and Box2D.</div>
+            <div className="text">Gassy Mcloud is a simple 'Flappy Bird' clone which was my first mobile project made in C++ using Cocos2D and Box2D.</div>
           </div>
         </div>
         <div class="container">
           <img className="image" alt="Mount Tikki" src={Tikki}/>
           <div class="overlay">
-          <a href="https://globalgamejam.org/2016/games/mount-tiki">
             <div className="header">Mount Tikki</div>
-            <div className="text">This is a test</div>
-          </a>
+            <div className="text-long">Mount Tiki was my group submission for the Southampton Global Game Jam  which is a fast paced, local co-op, free-for-all
+            brawler made in Unity using C#. In this game you have to try and capture and sacrifice the pig whilst avoiding other players.
+            Feel free to download the game <a href="https://globalgamejam.org/2016/games/mount-tiki">here.</a></div>
           </div>
         </div>
         <div class="container">
           <a><img className="image" alt="Fable Of Pickle" src={Fable}/></a>
           <div class="overlay">
             <div className="header">Fable of Pickle</div>
-            <div className="text">This is a test</div>
+            <div className="text-long">The Fable of Pickle is a dungeon crawler made in C++ and SDL 2.0. These are some of the core game mechanics this project covered:
+              Finite State Machines,
+              A* Pathfinding,
+              Reading and Writing to text files (Level Management),
+              Game Loop and Timers,
+              Collisions,
+              Audio.
+            </div>
           </div>
         </div>
         <div class="container">
